1f22fab8be2d20b33d4eeb10b4b68227a1213af3
ManiiSingh <manii.singh4@gmail.com>
2018-10-28 22:26:19 +0530

    Hello Selenium...

--- src/main/java/com/devopsmaventraning/payment/MyFirstJavaProgram.java
+++ src/main/java/com/devopsmaventraning/payment/MyFirstJavaProgram.java
@@ -5,7 +5,7 @@ public class MyFirstJavaProgram {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("Hello Selenium");
+		System.out.println("Hello Selenium...");
 	}
 
 }
